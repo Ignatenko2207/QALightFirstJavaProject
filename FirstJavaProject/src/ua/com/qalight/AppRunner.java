7e1b635033cdaab89f049f8a7ef93f6557e4bedd
Prime <kloreyn15@gmail.com>
2018-11-19 20:09:32 +0300

    FirstCommit

--- FirstJavaProject/src/ua/com/qalight/AppRunner.java
+++ FirstJavaProject/src/ua/com/qalight/AppRunner.java
@@ -1,8 +1,21 @@
 package ua.com.qalight;
 
+import java.util.logging.Logger;
+
+import ua.com.qalight.iserebryakov.User;
+
 public class AppRunner {
+	
+	static Logger Logger = logger.getLogger(AppRunner.class.getName());
 
 	public static void main(String[] args) {
-		System.out.println("out text");
+		
+		User userPrime = new User();	
+		userPrime.setUserId(12345);
+		userPrime.setUserName("Ilya");
+		userPrime.setUserSurname("Serebryakov");
+		userPrime.setAge(20);
+		
+		Logger.info("User" + userPrime.getUserName() + "has id" + userPrime.getUserId());
 	}
 }
