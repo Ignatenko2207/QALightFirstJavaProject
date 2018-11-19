2d64e164541c49b4c5846e39581008c8307244c1
vovkolga <vovkolga@macbook-vovk.dlink>
2018-11-19 20:08:26 +0200

    Vovk comit

--- FirstJavaProject/src/ua/com/qalight/AppRunner.java
+++ FirstJavaProject/src/ua/com/qalight/AppRunner.java
@@ -1,8 +1,25 @@
 package ua.com.qalight;
 
+
+import java.util.logging.Logger;
+
+import ua.com.qalight.vovk.User;
+
 public class AppRunner {
+	
+	static Logger logger = Logger.getLogger(AppRunner.class.getName());
+	
 
 	public static void main(String[] args) {
 		System.out.println("out text");
+		
+		User userOlga = new User();
+		userOlga.setUserId(1997);
+		userOlga.setUreSurname("Vovk");
+		userOlga.setAge(21);
+		userOlga.setUserName("Olga");
+		
+		logger.info("User" + userOlga.getUserName() + "has Id" + userOlga.getUserId());
+		
 	}
 }
---
+++ FirstJavaProject/src/ua/com/qalight/vovk/User.java
@@ -0,0 +1,46 @@
+package ua.com.qalight.vovk;
+
+public class User {
+
+	private String userName;
+	
+	private String ureSurname;
+
+	private Integer age;
+	
+	private int userId;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUreSurname() {
+		return ureSurname;
+	}
+
+	public void setUreSurname(String ureSurname) {
+		this.ureSurname = ureSurname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
+}
