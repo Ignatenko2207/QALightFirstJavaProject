b95aba7302c23de04f0fc745c4f585f5623e4e40
Alex <ignatenko2207@gmail.com>
2018-11-19 19:55:56 +0200

    Start new project

--- FirstJavaProject/src/ua/com/qalight/AppRunner.java
+++ FirstJavaProject/src/ua/com/qalight/AppRunner.java
@@ -1,8 +1,42 @@
 package ua.com.qalight;
 
+import java.util.logging.Logger;
+
+import ua.com.qalight.ignatenko.User;
+
 public class AppRunner {
 
+	static Logger logger = Logger.getLogger(AppRunner.class.getName());
+	
 	public static void main(String[] args) {
-		System.out.println("out text");
+		
+		User userAlex = new User();
+		userAlex.setUserId(12345);
+		userAlex.setUserName("Alex");
+		userAlex.setUserSurname("Ignatenko");
+		userAlex.setAge(35);
+		
+		logger.info("User " + userAlex.getUserName() + " has id " + userAlex.getUserId());
+		
+		User userPupkin = new User();
+		userPupkin.setUserId(12346);
+		userPupkin.setUserName("Vasil");
+		userPupkin.setUserSurname("Pupkin");
+		userPupkin.setAge(33);
+		
+		logger.info("User " + userPupkin.getUserName() + " has id " + userPupkin.getUserId());
+
+ 	}
+	
+	private void printSomething(String text)
+	{
+		System.out.println(text);
+	}
+	
+	private void printSomething(String text, Integer iterator)
+	{
+		for (int i = 0; i < iterator; i++) {
+			System.out.println(text);
+		}
 	}
 }
---
+++ FirstJavaProject/src/ua/com/qalight/ignatenko/User.java
@@ -0,0 +1,46 @@
+package ua.com.qalight.ignatenko;
+
+public class User {
+
+	private String userName;
+	
+	private String userSurname;
+	
+	private Integer age;
+	
+	private int userId;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserSurname() {
+		return userSurname;
+	}
+
+	public void setUserSurname(String userSurname) {
+		this.userSurname = userSurname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
+}
