30b7b785f9689762aff7ac5f707631ce07e9b917
Anastasiia Sedko <nastena.sedko@gmail.com>
2018-11-19 20:08:42 +0200

    Sedko commit

--- FirstJavaProject/src/ua/com/qalight/AppRunner.java
+++ FirstJavaProject/src/ua/com/qalight/AppRunner.java
@@ -1,8 +1,22 @@
 package ua.com.qalight;
 
+import ua.com.qalight.sedko.User;
+
+import java.util.logging.Logger;
+
 public class AppRunner {
 
-	public static void main(String[] args) {
-		System.out.println("out text");
-	}
+    static Logger logger = Logger.getLogger(AppRunner.class.getName());
+
+    public static void main(String[] args) {
+
+        User userAnastasiia = new User();
+
+        userAnastasiia.setUserId(44444);
+        userAnastasiia.setUserName("Anastasiia");
+        userAnastasiia.setUserSurname("Sedko");
+        userAnastasiia.setAge(18);
+
+        logger.info("User " + userAnastasiia.getUserName() + " has id " + userAnastasiia.getUserId());
+    }
 }
