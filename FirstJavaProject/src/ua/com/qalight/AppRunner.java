9e6ad3c5f05a53e86fcd84f96e30dd000a32243b
user <user@DESKTOP-SFVMRT7.Dlink>
2018-11-21 20:27:30 +0200

    GH

--- FirstJavaProject/src/ua/com/qalight/AppRunner.java
+++ FirstJavaProject/src/ua/com/qalight/AppRunner.java
@@ -1,11 +1,9 @@
 package ua.com.qalight;
 
-//import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Queue;
-//import java.util.Set;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
-//import ua.com.qalight.pylypchuk.User;
+import ua.com.qalight.pylypchuk.service.Randomizer;
 
 public class AppRunner {
 
@@ -13,36 +11,25 @@ public class AppRunner {
 	
 	public static void main(String[] args) {
 		
-//		Integer[] rooms = new Integer[38];
-//		String str = "64";
-//		char[] symbols = str.toCharArray();
-//		rooms[37] = 38;
-//		rooms[0] = 1;
-//		logger.info(String.valueOf(rooms[rooms.length-1]));
-//		logger.info(String.valueOf(rooms[36]));
-//		   rooms.add(1);
-//		   rooms.add(2);
-//		   rooms.add(3);
-//		   rooms.add(4);
-//		   rooms.add(5);
+		Set<String> emails = new HashSet<>();
 		
-//	   Set<Integer> rooms = new HashSet<>();
-//	   
-//	   for(int i = 0;i<38; i++) {
-//		   rooms.add(i);
-//	   }
-//		List<Integer> rooms = new ArrayList<>();
-//		   
-//		   for(int i = 0;i<38; i++) {
-//			   rooms.add(i);
-//		   }
-		Queue<Integer> rooms = new PriorityQueue<>();
-		   
-		   for(int i = 0;i<38; i++) {
-			   rooms.add(i);
+		for (int i = 0; i < 10; i++) {
+			String email = Randomizer.getRandomString(Randomizer.getRandomInteger(8, 16));
+			
+			email += "@" + Randomizer.getRandomDomain(Randomizer.getRandomInteger(1, 5));
+			
+			emails.add(email);
+		}
+			for (String email : emails) {
+				logger.info(emails);
+				
+			}
+		}
+		
+
 			   
 		   }
 	   
-	   logger.info(String.valueOf(rooms.size()));
-	}
-}
+	  
+	
+
