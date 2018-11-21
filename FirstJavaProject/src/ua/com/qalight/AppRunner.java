8cf094fd02a759073a923edf9cf28f0dbccd341a
Prime <kloreyn15@gmail.com>
2018-11-21 19:16:27 +0300

    MyCommit

--- FirstJavaProject/src/ua/com/qalight/AppRunner.java
+++ FirstJavaProject/src/ua/com/qalight/AppRunner.java
@@ -1,21 +1,62 @@
 package ua.com.qalight;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
-import ua.com.qalight.iserebryakov.User;
+//import ua.com.qalight.iserebryakov.User;
 
 public class AppRunner {
 	
-	static Logger Logger = logger.getLogger(AppRunner.class.getName());
-
+	static Logger logger = Logger.getLogger(AppRunner.class.getName());
+	
 	public static void main(String[] args) {
 		
-		User userPrime = new User();	
-		userPrime.setUserId(12345);
-		userPrime.setUserName("Ilya");
-		userPrime.setUserSurname("Serebryakov");
-		userPrime.setAge(20);
+		Set <Integer> rooms = new HashSet<>();
 		
-		Logger.info("User" + userPrime.getUserName() + "has id" + userPrime.getUserId());
+		rooms.add(1);
+		rooms.add(2);
+		rooms.add(3);
+		rooms.add(4);
+		rooms.add(5);
+		rooms.add(6);
+		
+	for(int i = 0; i < 38; i=i+1) {
+		rooms.add(i);
 	}
+	logger.info(String.valueOf(rooms.size()));
+}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+//	private static Logger logger = Logger.getLogger(AppRunner.class.getName());
+//
+//	public static void main(String[] args) {
+//		
+//		int[] rooms = new int[38];
+//		
+//		rooms[38] = 38;
+//		
+//		rooms[0] = 1;
+//		
+//		String str = "64";
+//		
+//		char[] symbols = str.toCharArray();
+//		
+//		Logger.info(String.valueOf(rooms[rooms.length-1]));
+//		Logger.info(String.valueOf(rooms[36]));
+		
+//		User userPrime = new User();	
+//		userPrime.setUserId(12345);
+//		userPrime.setUserName("Ilya");
+//		userPrime.setUserSurname("Serebryakov");
+//		userPrime.setAge(20);
+//		
+//		Logger.info("User" + userPrime.getUserName() + "has id" + userPrime.getUserId());
 }
